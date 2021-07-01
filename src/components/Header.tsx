782f640141d461788d4c82a2e6553b439511fcd4
201411108 <aggsae@gmail.com>
2021-07-01 23:45:30 +0900

    구현: 반응형 Header 구현
    PC, tablet, phone 화면 대응을 위한 반응형 웹 적용
    
    * src/components/Home.css
    - Header의 모든 요소에 대해서 미디어 쿼리 적용
    * src/components/Home.tsx
    - 미사용 모듈 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
 import { AroundRowContainer,  
-         ColumnContainer, StyledButton, StyledLogo, StyledSpan, StyledSearchBar, StyledSearchInput, StyledSearchImg } from './style'
+         ColumnContainer, StyledLogo, StyledSpan, StyledSearchBar, StyledSearchInput, StyledSearchImg } from './style'
 
 
 function Header() {
@@ -23,28 +23,30 @@ function Header() {
                     </button>
                 </Link>
             </div>
-            <AroundRowContainer>
-                <ColumnContainer>
-                    <StyledLogo src='/assets/remoters.webp' 
-                                srcSet='/assets/remoters@2x.webp 2x,
-                                        /assets/remoters@3x.webp 3x'
-                                alt='logo' />
-                    <StyledSpan>[명사] 시간과 공간의 제약을 받지않고 일하는 사람</StyledSpan>
-                    <StyledSearchBar>
-                        <StyledSearchInput type='text' />
-                        <StyledSearchImg src="/assets/group-9.webp"
-                            srcSet="/assets/group-9@2x.webp 2x,
+            <div className='aroundRowContainer'>
+                <div className='columnContainer'>
+                    <img className='mainLogo'
+                         src='/assets/remoters.webp' 
+                         srcSet='/assets/remoters@2x.webp 2x,
+                                 /assets/remoters@3x.webp 3x'
+                         alt='logo' />
+                    <span className='logoSpan'>[명사] 시간과 공간의 제약을 받지않고 일하는 사람</span>
+                    <span className='searchBarContainer'>
+                        <input className='searchInput' type='text' />
+                        <img className='searchImage'
+                             src="/assets/group-9.webp"
+                             srcSet="/assets/group-9@2x.webp 2x,
                                     /assets/group-9@3x.webp 3x"
-                            alt="Group-9"
-                            onClick={() => alert('검색 버튼 눌림')}/>
-                    </StyledSearchBar>
-                </ColumnContainer>
-                <img src="/assets/wfh-1.webp"
+                             alt="Group-9"
+                             onClick={() => alert('검색 버튼 눌림')}/>
+                    </span>
+                </div>
+                <img className='mainImage'
+                     src="/assets/wfh-1.webp"
                      srcSet="/assets/wfh-1@2x.webp 2x,
                             /assets/wfh-1@3x.webp 3x"
                      alt="wfh"/>
-
-            </AroundRowContainer>
+            </div>
         </>
     )
 }
