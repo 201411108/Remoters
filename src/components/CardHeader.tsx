a194ca1344a4752dfea54006ae47a9f5223c4efa
201411108 <aggsae@gmail.com>
2021-07-04 21:09:49 +0900

    리팩토링: CardHeader styled-component 제거
    styled-component 미사용에 따른 리팩토링
    
    ISSUE#4 : 라벨과 체크박스 사이의 공간이 띄워지지 않는 문제

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react'
-import { CardAroundRowContainer, CardRowContainer, StyledCheckboxLabel } from './style'
+import './CardHeader.css'
+import { StyledCheckboxLabel } from './style'
 
 
 interface cardHeaderProps {
@@ -17,18 +18,18 @@ export function CardHeader({ title }: cardHeaderProps) {
 
     return (
         <>
-            <CardAroundRowContainer>
+            <div className='cardAroundRowContainer'>
                 <h3>{title}</h3>
-                <CardRowContainer>
+                <div className='cardRowContainer'>
                     { !isCheck ? (
                             <img src='/assets/group-3.webp' alt='check' onClick={ onClick }/>
                         ) : (
                             <img src='/assets/group-3-1.webp' alt='check' onClick={ onClick }/>
                         )  
                     }
-                    <StyledCheckboxLabel>Non-IT 잡만 보기</StyledCheckboxLabel>
-                </CardRowContainer>
-            </CardAroundRowContainer>
+                    <span className='label'>Non-IT 잡만 보기</span>
+                </div>
+            </div>
         </>
     )
 }
