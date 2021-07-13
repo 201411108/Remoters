9ff02f0c1fdee48a53910380529b1ccf303c7351
201411108 <aggsae@gmail.com>
2021-07-13 23:23:59 +0900

    픽스: CardHeader의 width가 좁게 나오는 문제 해결 - close #7
    CardHeader의 width값이 잡히지 않아 제대로 표시되지 않는 문제 해결
    
    * src/pages/Home.css
    - columnContainer의 width를 100%로 수정
    * src/components/CardHeader.css
    - space-between : 너무 옆으로 퍼지는 디자인
    - space-between 속성을 space-around로 수정

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -22,9 +22,17 @@ export function CardHeader({ title }: cardHeaderProps) {
                 <h3>{title}</h3>
                 <div className='cardRowContainer'>
                     { !isCheck ? (
-                            <img src='/assets/group-3.webp' alt='check' onClick={ onClick }/>
+                            <img src='/assets/group-3.webp' 
+                                 srcSet='/assets/group-3@2x.webp 2x
+                                         /assets/gruop-3@3x.webp 3x'
+                                 alt='check'
+                                 onClick={ onClick }/>
                         ) : (
-                            <img src='/assets/group-3-1.webp' alt='check' onClick={ onClick }/>
+                            <img src='/assets/group-3-1.webp' 
+                                 srcSet='/assets/group-3-1@2x.webp 2x
+                                         /aasets/group-3-1@3x.webp 3x'
+                                 alt='check'
+                                 onClick={ onClick }/>
                         )  
                     }
                     <span className='label'>Non-IT 잡만 보기</span>
